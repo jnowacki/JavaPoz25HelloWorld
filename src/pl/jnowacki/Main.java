e3e6d398e841190a5b160d285fc50ddfea586b41
Jedrzej Nowacki <contact@jnowacki.pl>
2020-02-29 12:17:03 +0100

    Inital commit

---
+++ src/pl/jnowacki/Main.java
@@ -0,0 +1,11 @@
+package pl.jnowacki;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person piotrek = new Person();
+
+        piotrek.sayHello();
+        piotrek.sayAge();
+    }
+}
---
+++ src/pl/jnowacki/Person.java
@@ -0,0 +1,14 @@
+package pl.jnowacki;
+
+public class Person {
+    public int age = 8;
+
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+    public void sayAge(){
+        System.out.println("I'm");
+        System.out.println(age);
+    }
+}
